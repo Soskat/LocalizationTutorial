058b3cba6da0192bc47c8f9875fc2749bac1bc22
K8 Sosnowska <sosnowska.kk@gmail.com>
2022-02-12 18:37:13 +0100

    Added one example for creating String Table

--- LocalizationTutorial/Source/LocalizationTutorial/LocalizationTutorial.cpp
+++ LocalizationTutorial/Source/LocalizationTutorial/LocalizationTutorial.cpp
@@ -1,6 +1,7 @@
 #include "LocalizationTutorial.h"
 #include "Modules/ModuleManager.h"
 #include "LocalizationTutorial/MainGame/CsvBaseStringTableLoaderMainGame.h"
+#include "LocalizationTutorial/MainGame/StringTableGeneratorMainGame.h"
 #include "LocalizationTutorial/SomeDLC/CsvBaseStringTableLoaderSomeDLC.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FLocalizationTutorial, LocalizationTutorial, "LocalizationTutorial" );
@@ -9,6 +10,8 @@ void FLocalizationTutorial::StartupModule()
 {
 	FDefaultGameModuleImpl::StartupModule();
 
+	// Create new String Table using LOCTABLE family of macros:
+	MainGameLocalization::GenerateWeaponsStringTable();
 	// Create new String Tables for localized texts using the data loaded from CSV files:
 	MainGameLocalization::LoadLocalizedTextsFromCsv();
 	SomeDLCLocalization::LoadLocalizedTextsFromCsv();
